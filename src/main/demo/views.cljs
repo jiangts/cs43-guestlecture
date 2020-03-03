e3d7632297e6ba3cdf4008f1850823a6b306946f
Allan Jiang <origami789@gmail.com>
2020-03-02 16:43:52 -0800

    add piece images

--- src/main/demo/views.cljs
+++ src/main/demo/views.cljs
@@ -1,12 +1,24 @@
 (ns demo.views
-  (:require [reagent.core :as reagent]
-            [re-frame.core :as re-frame]
+  (:require [reagent.core :as r]
             [cljs-bean.core :refer (bean ->clj ->js)]
             [better-cond.core :as b]))
 
+
+(defn board-inner [props]
+  (let [state (r/atom nil)
+        id (str (gensym "board"))]
+
+    (r/create-class
+      {:reagent-render (fn [props]
+                         [:div (merge props {:id id})])
+
+       :component-did-mount (fn [c]
+                              (let [board (js/Chessboard. id "start")]))
+       :component-did-update (fn [c]
+                               )
+       :display-name "board-inner"})))
+
+
 (defn app-root []
-  [:h3 "hi world"])
-
-
-;; var board = Chessboard('myBoard', 'start')
+  [board-inner {:style {:width 400}}])
 
